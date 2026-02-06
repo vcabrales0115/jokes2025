151d716da0e431bf88671ad289497968e0fdb49c
vcabrales0115 <vcabrales@cps.edu>
2026-02-05 21:20:43 -0600

    Refactor joke game for better user interaction

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 
+username= input("Please enter a username:")
+print(f"Hello,{username}!")
+
+def play_jokes():
+    
+    joke = input("Do you want to hear a joke? (yes/no): ").lower()
+    
+    if joke == "no":
+        print("Okay, suit yourself!")
+        return
+
+   
+    while joke == "yes":
+        topics = ["robbers", "tanks", "pencils"]
+        print(f"Available topics: {topics}")
+        
+        choice = input("Pick a topic: ").lower()
+
+        if choice == "robbers":
+            input("Knock Knock")
+            input("Calder")
+            print("Calder police - I've been robbed!")
+        elif choice == "tanks":
+            input("Knock Knock")
+            input("Tank")
+            print("You're welcome!")
+        elif choice == "pencils":
+            input("Knock Knock")
+            input("Broken pencil")
+            print("Nevermind, it's pointless!")
+        else:
+            print("That topic isn't in the list!")
+
+        joke = input("Do you want to hear another joke or are you finished? (yes/finished): ").lower()
+
+    # Feedback section
+    if joke == "finished":
+        try:
+            rate = int(input("Please rate our game 1-10: "))
+            final_score = rate * 10
+            print(f"{final_score}% satisfaction rate!")
+        except ValueError:
+            print("Thanks for playing!")
+
+        friend = input("Would you recommend this game to a friend? (yes/no): ").lower()
+        if friend == "yes" or friend == "maybe":
+            print("Thanks, we appreciate it!")
+        else:
+            print("Sorry you didn't enjoy it.")
 
 
-# make this performance task ready for submission
-# To give the user a fun experience hearing knock knock jokes
+play_jokes()
 
-joke = input("Do you want to hear a joke? ")
-if joke == "no":
-    print("Okay suit yourself!")
-while joke == "yes":
-    print("Great, Let's Play")
-    question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
-    if question == "robbers":
-        input("Knock Knock ")
-        input("Calder")
-        print("Calder police - I've been robbed!")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "tanks":
-        input("Knock Knock ")
-        input("Tank ")
-        input("You are welcome! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "pencils":
-        input("Knock Knock ")
-        input("Broken pencil ")
-        input("Nevermind, it's pointless! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-if joke == "finished":
-    rate = int(input("Please rate our game 1-10! "))
-    final_score = int(rate * 10)
-    print(str(final_score) + " percent satisfaction rate")
-    friend = input("Would you recommend this game to a friend? ")
 
-    if friend == "yes" or friend == "maybe":
-        print("Thanks, we appreciate it. ")
-    else:
-        print("Sorry you did not enjoy it. ")
